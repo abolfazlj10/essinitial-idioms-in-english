3c3a38d8c3e7665aba8fb0a983de464dfdc42cda
abolfazl <abolfazljamshidi101010@gmail.com>
2025-06-17 10:07:35 +0330

    first layout of first cart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <div className="relative">
+          <img className="w-[800px] absolute opacity-20 -bottom-32 -left-60 select-none" src="/blob3.svg"/>
+          <img className="w-[500px] absolute opacity-80 select-none" src="/blob2.svg"/>
+          <img className="w-[350px] absolute right-32 bottom-0 opacity-50 select-none" src="/blob2.svg"/>
+          <div className="h-dvh flex items-center justify-center py-10 z-20 relative">
+            <div className="display w-[1800px] max-h-[1000px] border-4 h-full bg-[#fff] shadow-2xl rounded-2xl text-black p-9 flex flex-col gap-14">
+              {children}
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,37 +30,33 @@ export default function Home() {
     }
   ]);
   return (
-    <div className="relative">
-      <img className="w-[800px] absolute opacity-20 -bottom-32 -left-60" src="/blob3.svg"/>
-      <img className="w-[500px] absolute opacity-80" src="/blob2.svg"/>
-      <img className="w-[350px] absolute right-32 bottom-0 opacity-50" src="/blob2.svg"/>
-      <div className="h-dvh flex items-center justify-center py-10 z-20 relative">
-        <div className="display w-[1800px] max-h-[1000px] border-4 h-full bg-[#fff] shadow-2xl rounded-2xl text-black p-9 flex flex-col gap-10">
-          <div className="flex flex-col gap-6 text-lg">
-            <div className="text-[80px] font-bold ">Leran essenitial idioms with <span className="bg-gradient-to-r from-[#4e5996] to-primaryColor bg-clip-text text-transparent">AI</span></div>
+    <>
+      <div className="flex flex-col gap-6 text-lg">
+        <div className="text-[80px] font-bold select-none">Leran essenitial idioms with <span className="bg-gradient-to-r from-[#4e5996] to-primaryColor bg-clip-text text-transparent">AI</span></div>
+      </div>
+      <div className="flex-1 flex">
+        <div className="flex-1 flex flex-col">
+          <div className="text-lg">
+            This <span className="bg-hilightColor/30 px-1">AI-powered tool</span> is designed to help you master <span className="bg-hilightColor/30 px-1">essential English idioms</span> in a fun and effective way. With features like the Story Creator, Flash Cards, and a full Book of idioms, it makes learning both <span className="bg-hilightColor/30 px-1">interactive</span> and practical. Whether you're a beginner or looking to improve your fluency, this tool supports your progress by offering structured lessons and <span className="bg-hilightColor/30 px-1">personalized practice</span>. You'll build your vocabulary, understand real-life usage, and <span className="bg-hilightColor" />
+            <span className="bg-hilightColor/30 px-1">gain confidence</span> in speaking and writing. The Archives section also helps you save and review what you've learned. It's an ideal companion for daily English improvement.
           </div>
-          <div className="flex-1 flex">
-            <div className="flex-1 flex items-center">
-            <PiDotOutlineFill className="text-3xl" /> with this platform you can learn idioms easyerliy
-            </div>
-            <div className="flex-1 flex justify-center items-center">
-              <div className="ring-2 ring-primaryColor rounded-xl w-[700px] relative">
-                <img className="w-full rounded-xl h-full object-cover" src="./Screenshot 2025-06-16 103128.png" />
-                <img className="absolute -top-6 -right-10" src="./icon/Direct Hit.svg"/>
-              </div>
-            </div>
-          </div>
-          <div className="font-bold flex flex-col gap-10">
-            <div className="text-xl">Quick Access <img className="w-[30px] inline-block" src="./icon/Backhand Index Pointing Down Medium Skin Tone.svg" /></div>
-            <div className="flex pr-40 gap-5">
-              {quickAccess.map((item,id)=>(
-                <QuickAccessCard key={id} icon={item.icon} title={item.title} description={item.description} />
-              ))}
-            </div>
+        </div>
+        <div className="flex-1 flex justify-center items-center select-none">
+          <div className="ring-3 ring-primaryColor rounded-xl w-[700px] h-full relative">
+            <img className="w-full rounded-xl h-full object-cover shadow-xl shadow-bgColor" src="./Screenshot 2025-06-16 103128.png" />
+            <img className="absolute -top-9 -right-10" src="./icon/Direct Hit.svg"/>
           </div>
         </div>
       </div>
-    </div>
+      <div className="font-bold flex flex-col gap-10">
+        <div className="text-xl select-none">Quick Access <img className="w-[30px] inline-block" src="./icon/Backhand Index Pointing Down Medium Skin Tone.svg" /></div>
+        <div className="flex pr-40 gap-5">
+          {quickAccess.map((item,id)=>(
+            <QuickAccessCard key={id} icon={item.icon} title={item.title} description={item.description} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -79,7 +75,7 @@ export function QuickAccessCard({icon, title, description}: QuickAccessCardProps
       <div className="text-justify space-y-3">
         <div>{title}</div>
         <div className="text-sm text-gray-500 font-medium pb-2">{description}</div>
-        <div className="text-sm border text-primaryColor rounded-lg p-2 w-1/2 text-center shadow cursor-pointer hover:bg-primaryColor hover:text-white duration-100">Learn more =></div>
+        <div className="select-none text-sm border text-primaryColor rounded-lg p-2 w-1/2 text-center shadow cursor-pointer hover:bg-primaryColor hover:text-white duration-100">Learn more =></div>
       </div>
     </div>
   );
