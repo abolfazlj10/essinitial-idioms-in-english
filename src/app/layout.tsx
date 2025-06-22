39d2a257f7c81451b6b086d5c19d209f289025d4
abolfazl <abolfazljamshidi101010@gmail.com>
2025-06-22 11:10:46 +0330

    section lesson and words

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="relative">
-          <img className="w-[800px] absolute opacity-20 -bottom-32 -left-60 select-none" src="/blob3.svg"/>
-          <img className="w-[500px] absolute opacity-80 select-none" src="/blob2.svg"/>
-          <img className="w-[350px] absolute right-32 bottom-0 opacity-50 select-none" src="/blob2.svg"/>
-          <div className="h-dvh flex items-center justify-center py-10 z-20 relative">
-            <div className="display w-[1800px] max-h-[1000px] border-4 h-full bg-[#fff] shadow-2xl rounded-2xl text-black p-9 flex flex-col gap-14 relative">
-              {children}
-            </div>
+        <div className="relative min-h-screen max-h-screen min-w-screen max-w-screen h-screen w-screen flex justify-center items-center py-22 z-10">
+          <img className="z-10 w-[800px] absolute opacity-20 -bottom-32 -left-60 select-none" src="/blob3.svg"/>
+          <img className="z-10 w-[500px] absolute left-0 top-0 opacity-80 select-none" src="/blob2.svg"/>
+          <img className="z-10 w-[350px] absolute right-32 bottom-0 opacity-50 select-none" src="/blob2.svg"/>
+          <div className="relative h-full w-full mx-60 border-4 bg-[#fff] shadow-2xl rounded-2xl text-black p-9 z-20">
+            {children}
           </div>
         </div>
       </body>
--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -27,7 +27,7 @@ export default function Story () {
         // test()
     })
     return(
-        <div className="h-full flex flex-col gap-5">
+        <div className="h-full flex flex-col gap-5 overflow-hidden">
             <div className="flex gap-5 select-none">
                 <div className="flex-1 flex flex-col gap-2">
                     <div className="w-full h-[8px] bg-gradient-to-r from-primaryColor from-40% to-bgColor rounded"></div>
@@ -42,8 +42,8 @@ export default function Story () {
                     <div className="text-gray-400">Words</div>
                 </div>
             </div>
-            <div className="grid grid-cols-[6fr_2fr] gap-10 h-full">
-                <div className="flex flex-col gap-9">
+            <div className="grid grid-cols-[7fr_2fr] gap-10 flex-1 overflow-hidden">
+                <div className="flex flex-col gap-9 overflow-hidden">
                     <div className="flex flex-col gap-3 select-none">
                         <div className="text-[30px] font-semibold">Select Level</div>
                         <div className="text-gray-400 text-lg">Select your level that you wanna see its words</div>
@@ -65,6 +65,22 @@ export default function Story () {
                             <div className="text-gray-400 text-lg">Description in one line</div>
                         </div>
                     </div>
+                    <div className="flex flex-col gap-8 flex-1 overflow-hidden">
+                        <div className="flex flex-col gap-3 select-none">
+                            <div className="text-[30px] font-semibold">Select Words</div>
+                            <div className="text-gray-400 text-lg">Select your words after that you selected the lesson</div>
+                        </div>
+                        <div className="flex flex-1 border rounded-xl p-2 overflow-hidden">
+                            <div className="overflow-y-scroll h-full flex-1 flex flex-col gap-5 px-2">
+                                {elementry.levels[0].lessons.map((item,key)=>(
+                                    <div className="border rounded p-2" key={key}>Lesson {item.lesson_number}</div>
+                                ))}
+                            </div>
+                            <div className="p-2 flex-1">
+                                <div className="text-lg font-semibold">Words</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="border border-gray-400/10 rounded-xl relative overflow-hidden shadow-xl">
                     <img className="absolute top-1/2 -right-20 z-20 scale-x-150" src="./blob-haikei.svg" />
@@ -72,11 +88,11 @@ export default function Story () {
                     <div className="bg-white/30 h-full w-full backdrop-blur-2xl z-30 relative py-7 px-5 flex flex-col gap-10">
                         <div>
                             <div className="border-4 backdrop-blur-2xl justify-self-start py-1 px-4 font-semibold rounded-xl bg-blue-500/50 -mb-5 -ml-4 z-20 relative select-none">Level :</div>
-                            <div className="text-[30px] font-semibold text-center rounded-xl bg-white/20 border py-2">Elementry</div>
+                            <div className="text-[25px] font-semibold text-center rounded-xl bg-white/20 border py-2">Elementry</div>
                         </div>
                         <div>
                             <div className="border-4 backdrop-blur-2xl justify-self-start py-1 px-4 font-semibold rounded-xl bg-blue-500/50 -mb-5 -ml-4 z-20 relative select-none">Lesson :</div>
-                            <div className="text-[30px] font-semibold text-center rounded-xl bg-white/20 border py-2">Lesson 13</div>
+                            <div className="text-[25px] font-semibold text-center rounded-xl bg-white/20 border py-2">Lesson 13</div>
                         </div>
                         <div>
                             <div className="border-4 backdrop-blur-2xl justify-self-start py-1 px-4 font-semibold rounded-xl bg-blue-500/50 -mb-5 -ml-4 z-20 relative select-none">Words :</div>
