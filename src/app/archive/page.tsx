39d20681a9e4045f8336ee237245c5a2c80c6780
abolfazl <abolfazljamshidi101010@gmail.com>
2025-07-26 11:54:53 +0330

    correct coming soon text

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -2,7 +2,7 @@ export const Archive = () => {
     return(
         <div className="flex justify-center items-center h-full">
             <div className="text-4xl font-bold">
-                <span className="bg-gradient-to-r from-[#4e5996] to-primaryColor bg-clip-text text-transparent">Comming Soon...</span>
+                <span className="bg-gradient-to-r from-[#4e5996] to-primaryColor bg-clip-text text-transparent">coming soon...</span>
             </div>
         </div>
     )
